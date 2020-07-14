761e0bd282334e6fc489d959a900881731a3e781
Peter NTSUNTSHA <pntsunts@c4r9s8.wethinkcode.co.za>
2020-07-14 12:40:13 +0200

    Update

--- ft_echo.c
+++ ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: pntsunts <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/13 09:32:57 by pntsunts          #+#    #+#             */
-/*   Updated: 2020/07/13 16:56:56 by pntsunts         ###   ########.fr       */
+/*   Updated: 2020/07/14 11:28:49 by pntsunts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,75 +21,34 @@ static int		compare(char *s1, char *s2)
 	{
 		if (s1[i] == s2[i])
 		{
-			return (1);
-			break;
+			return (1);;
 		}
 		i++;
 	}
 	return (0);
 }
 
-
-/*char	*Get_path(char *str)
+char	*Get_path(char *str)
 {
-	int i = 1;
+	int i = 0;
 	char *tmp;
-
+	ft_putendl(str);
 	while (Data[i])
 	{
 		tmp = ft_strjoin(str, "=");
 		ft_putendl(tmp);
 		if (compare(Data[i], tmp) == 1)
 		{
-			//ft_putendl("HERE NOW");
-			return (ft_strchr(Data[i], '='));
-		}
-		i++;
-//		ft_putendl(Data[x++]);
-		i++;
-	}
-	return (NULL);
-}*/
-char	*Get_path(char *str)
-{
-	//ft_putstr(str);
-	int i = 0;
-	char *tmp;
-	tmp = str;
-
-	while (Data[i])
-	{
-		tmp = ft_strjoin(str, "=");
-		if (compare(Data[i], tmp) == 1)
-		{
-			return (ft_strchr(Data[i], '=') + 1);
-		}
-		else
-		{
-			ft_putendl("NOT FOUND");
+			//return (ft_strchr(Data[i], '=') + 1);
 		}
 		i++;
 	}
 	return (NULL);
-}
-static void paths(char *str)
-{
-	int i = 0;
-
-	char *path;
-	if (str[0] == '$')
-	{
-		path = Get_path(str + 1);
-	}
-	else
-	{
-		write(1, &str[i++], 1);
-	}
 }
 
 int ft_echo(char **str)
 {
-	//char *path;
+	char *path;
 	int i;
 
 	i = 1;
@@ -100,15 +59,12 @@ int ft_echo(char **str)
 	}
 	while (str[i])
 	{
-		/*if (str[i][0] == '$')
+		if (str[i][0] == '$')
 		{
-			path = Get_path(str[i++]);
+			path = Get_path(str[i++] + 1);
 			continue ;
 		}
 		ft_putstr(str[i++]);
-		*/
-		paths(str[i]);
-		i++;
 		ft_putchar(' ');
 	}
 	ft_putchar('\n');
