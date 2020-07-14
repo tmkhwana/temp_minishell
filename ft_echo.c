82d2cac859cead5499943b5fb725dafc6ec6a1c5
Peter NTSUNTSHA <pntsunts@c5r11s7.wethinkcode.co.za>
2020-07-14 13:48:48 +0200

    UPDATE

--- ft_echo.c
+++ ft_echo.c
@@ -6,40 +6,51 @@
 /*   By: pntsunts <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/13 09:32:57 by pntsunts          #+#    #+#             */
-/*   Updated: 2020/07/14 11:28:49 by pntsunts         ###   ########.fr       */
+/*   Updated: 2020/07/14 13:47:50 by pntsunts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int		compare(char *s1, char *s2)
+/*static char *ft_take(const char *str, int i)
 {
-	int i;
-
-	i = 0;
-	while (s2[i])
+	while (*str)
 	{
-		if (s1[i] == s2[i])
-		{
-			return (1);;
-		}
+		if (*str == i)
+			return ((char *)str);
 		i++;
 	}
 	return (0);
+}*/
+
+static int		compare(char *str1, char *str2)
+{
+	int i;
+
+	i = -1;
+	while (str2[++i])
+	{
+		if (str1[i] != str2[i])
+		{
+			return (0);
+		}
+	}
+	return (1);
 }
 
 char	*Get_path(char *str)
 {
 	int i = 0;
 	char *tmp;
-	ft_putendl(str);
+	char *store;
+
 	while (Data[i])
 	{
 		tmp = ft_strjoin(str, "=");
-		ft_putendl(tmp);
 		if (compare(Data[i], tmp) == 1)
 		{
-			//return (ft_strchr(Data[i], '=') + 1);
+			store = ft_strchr(Data[i], '=');
+			ft_putstr(store + 1);
 		}
 		i++;
 	}
